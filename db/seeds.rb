17915a84fa6c0875e5ab62877ae336f31b169b6e
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-13 10:42:57 -0700

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 10.times do
-  name: Faker::BackToTheFuture.character
-  age: rand(10...80)
-  hair_color: Faker::Color.color_name
-  eye_color: Faker::Dune.character
-  gender: Faker::Lorem.word
-  alive: rand(2) > 0
+  Animal.create(
+  name: Faker::BackToTheFuture.character,
+  age: Faker::Number.between(1,10),
+  hair_color: Faker::Color.color_name,
+  eye_color: Faker::Dune.character,
+  gender: Faker::Lorem.word,
+  alive: Faker::Boolean.boolean(0.7)
+  )
 end
 
 puts 'seeded'
